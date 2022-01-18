ca8de45c44f634f2d2597c1f1b42aae56712e04c
Lelton Borges <leltondev@gmail.com>
2022-01-18 15:26:05 -0300

    Project hr-payroll created

---
+++ ms_hr-payroll/src/main/java/br/com/ms/hrpayroll/MsHrPayrollApplication.java
@@ -0,0 +1,13 @@
+package br.com.ms.hrpayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsHrPayrollApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsHrPayrollApplication.class, args);
+    }
+
+}
---
+++ ms_hr-payroll/src/test/java/br/com/ms/hrpayroll/MsHrPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ms.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsHrPayrollApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

cfd984bee9179df437923660bf363580dbb09754
Lelton Borges <leltondev@gmail.com>
2022-01-18 17:34:58 -0300

    Project hr-eureka created

---
+++ hr-eureka-serve/src/test/java/br/com/ms/hreurekaserve/HrEurekaServeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ms.hreurekaserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
